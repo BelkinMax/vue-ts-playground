02896465f5faa27e08cb77a12ad619df8b9e1519
Maxim Belkin <belkinurban@gmail.com>
2021-11-01 23:33:15 +0100

    feat: :sparkles: Script setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
+<script lang="ts" setup>
+import axios from 'axios';
+import { reactive } from "@vue/reactivity";
+import { onMounted, watchEffect } from '@vue/runtime-core';
+
+import HelloWorld from "@/components/HelloWorld.vue";
+
+const message = "Hello world";
+const state = reactive({
+  counter: 0,
+  joke: null,
+  avatarUrl: null
+})
+
+const resetCounter = () => {
+  state.counter = 0;
+};
+
+const getChuckJoke = async () => {
+  const { data } = await axios.get("https://api.chucknorris.io/jokes/random");
+
+  if (data.id) {
+    state.joke = data.value;
+    state.avatarUrl ??= data.icon_url;
+  }
+}
+
+const setCounter = () => {
+  setInterval(() => state.counter++, 1000);
+}
+
+watchEffect(() => {
+  console.log(state.counter);
+})
+
+onMounted(() => {
+  setCounter();
+  getChuckJoke()
+})
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>{{ message }}</h1>
+    <img v-if="state.avatarUrl" :src="state.avatarUrl" alt="Chuck" />
+    <HelloWorld :counter="state.counter" />
+    <p>{{ state.joke }}</p>
+    <div class="flex-container">
+      <button @click="resetCounter">Reset Counter</button>
+      <button @click="getChuckJoke">Get new joke</button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
-</script>
+<style lang="scss" scoped>
+.flex-container {
+  display: flex;
+  gap: 1rem;
+  width: 100%;
+  justify-content: center;
+}
+</style>
