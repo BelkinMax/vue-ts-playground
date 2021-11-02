9247faabdd945882ff2f091f933576519d821bb0
Maxim Belkin <belkinurban@gmail.com>
2021-11-03 00:11:24 +0100

    refactor: :recycle: Api call trycatch

--- src/store/jokes/actions.ts
+++ src/store/jokes/actions.ts
@@ -4,13 +4,20 @@ import { RootState } from "../types";
 import { JokesState } from "./types";
 
 export const actions: ActionTree<JokesState, RootState> = {
-  async fetchRandomJoke () {
+  async fetchRandomJoke() {
     this.commit("SET_JOKE_IS_LOADING", true);
 
-    const { data } = await axios.get("https://api.chucknorris.io/jokes/random");
-    this.commit("SET_JOKE", data);
-    this.commit("ADD_JOKE_NUMBER");
+    try {
+      const { data } = await axios.get(
+        "https://api.chucknorris.io/jokes/random"
+      );
 
-    this.commit("SET_JOKE_IS_LOADING", false);
-  }
-}
+      this.commit("SET_JOKE", data);
+      this.commit("ADD_JOKE_NUMBER");
+    } catch (e) {
+      console.error("Api error :(", e);
+    } finally {
+      this.commit("SET_JOKE_IS_LOADING", false);
+    }
+  },
+};
